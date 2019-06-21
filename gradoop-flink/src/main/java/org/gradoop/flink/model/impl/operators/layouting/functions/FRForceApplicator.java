e683159d5c8ffdf506e6e37afd5c9cf6ef001934
Daniel Baumgarten <incognym@googlemail.com>
2019-06-21 17:44:32 +0200

    Reworked simulated annealling #1

--- gradoop-flink/src/main/java/org/gradoop/flink/model/impl/operators/layouting/functions/FRForceApplicator.java
+++ gradoop-flink/src/main/java/org/gradoop/flink/model/impl/operators/layouting/functions/FRForceApplicator.java
@@ -25,18 +25,30 @@ import org.gradoop.flink.model.impl.operators.layouting.util.LVertex;
 import org.gradoop.flink.model.impl.operators.layouting.util.Vector;
 
 /** Applies forces to vertices.
+ * Uses simulated annealing with exponentially decreasing temperature.
+ * Confines new coordinates to the layouting-space
  *
  */
 public class FRForceApplicator extends
   RichJoinFunction<LVertex, Force, LVertex> {
+
   /** Width of the layouting-space */
-  private int width;
-  /** Height of the layouting-space */
-  private int height;
-  /** Parameter for FR-Algorithm. Here used for simulated annealing */
-  private double k;
-  /** Number of FR-iterations to perform. Here used for simulated annealing */
-  private int maxIterations;
+  private int layoutWidth;
+
+  /** Height of the layouting space */
+  private int layoutHeight;
+
+  /** Speed at which the cooling-schedule starts */
+  private double startSpeed;
+
+  /** Base of the exponentially-decreasing function for the speed */
+  private double base;
+
+  /** cache the last computed temperature and re-use if possible to reduce needed computing-power */
+  private int lastIteration = -1;
+
+  /** cache the last computed temperature and re-use if possible to reduce needed computing-power */
+  private double lastTemperature = 0;
 
   /** Create new FRForceApplicator
    *
@@ -46,25 +58,47 @@ public class FRForceApplicator extends
    * @param maxIterations Number of iterations the FR-Algorithm will have
    */
   public FRForceApplicator(int width, int height, double k, int maxIterations) {
-    this.width = width;
-    this.height = height;
-    this.k = k;
-    this.maxIterations = maxIterations;
+    this.layoutWidth = width;
+    this.layoutHeight = height;
+    this.startSpeed = Math.sqrt(width*width+height*height)/2.0;
+    double endSpeed = k / 10.0;
+    this.base = Math.pow(endSpeed/startSpeed,1.0/(maxIterations-1));
+  }
+
+  /** Calculate the desired speed for a given iteration
+   *
+   * @param iteration Iteration to calculate speed for
+   * @return Desired speed
+   */
+  public double speedForIteration(int iteration){
+    // cache last result to avoid costly pow
+    if (iteration != lastIteration){
+      lastTemperature = startSpeed * Math.pow(base,iteration);
+      lastIteration = iteration;
+    }
+    return lastTemperature;
+  }
+
+  /** Apply force to position. Honoring speed limit anmd layouting-area.
+   * MODIFIES given vectors!
+   *
+   * @param position The position to modify
+   * @param movement The desired movement
+   * @param speedLimit The speed limit for the movement
+   */
+  public void applyForce(Vector position, Vector movement, double speedLimit){
+    movement.mClamped(speedLimit);
+    position.mAdd(movement);
+    position.mConfined(0, layoutWidth - 1, 0, layoutHeight - 1);
   }
 
   @Override
   public LVertex join(LVertex first, Force second) throws Exception {
-    int iteration = getIterationRuntimeContext().getSuperstepNumber();
-    double tmp = width / 3.0;
-    double speedLimit = -(tmp / maxIterations) * iteration + tmp + (width / 200.0);
 
+    double speedLimit = speedForIteration(getIterationRuntimeContext().getSuperstepNumber());
     Vector movement = second.getValue();
-    movement.mClamped(speedLimit);
-
     Vector position = first.getPosition();
-    position.mAdd(movement);
-    position.mConfined(0, width - 1, 0, height - 1);
-
+    applyForce(position,movement,speedLimit);
     first.setPosition(position);
     return first;
   }
