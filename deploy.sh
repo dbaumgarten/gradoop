9a835b2e282b809a04b021cc6e6ea7d8e88a6174
Daniel Baumgarten <incognym@googlemail.com>
2019-09-18 14:45:21 +0200

    Removed files that sould not be merged

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-rsync -avh -e ssh gradoop-examples/target/gradoop-examples-0.5.0-SNAPSHOT.jar db32geta@bdclu1.informatik.intern.uni-leipzig.de:gradoop-examples-0.5.0-SNAPSHOT.jar
-
-rsync -avh -e ssh benchmark.sh db32geta@bdclu1.informatik.intern.uni-leipzig.de:benchmark.sh
-
-ssh db32geta@bdclu1.informatik.intern.uni-leipzig.de
